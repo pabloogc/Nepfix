08b12403b3affceb47e5c155009416d51619d601
Pablo Orgaz <pabloogc@gmail.com>
2015-01-19 13:49:32 +0100

    Thread safe implementation for in memory repository

--- server/src/main/java/com/nepfix/server/db/InMemoryBlueprintRepository.java
+++ server/src/main/java/com/nepfix/server/db/InMemoryBlueprintRepository.java
@@ -6,25 +6,42 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 @Repository
 public class InMemoryBlueprintRepository implements BlueprintRepository {
 
     private final HashMap<String, NepBlueprint> nepBlueprintHashMap = new HashMap<>();
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
     @Override public NepBlueprint findBlueprint(String id) {
-        NepBlueprint nepBlueprint = nepBlueprintHashMap.get(id);
-        if (nepBlueprint == null) {
-            throw new BlueprintNotFoundException();
+        lock.readLock().lock();
+        try {
+            NepBlueprint nepBlueprint = nepBlueprintHashMap.get(id);
+            if (nepBlueprint == null) {
+                throw new BlueprintNotFoundException();
+            }
+            return nepBlueprint;
+        } finally {
+            lock.readLock().unlock();
         }
-        return nepBlueprint;
     }
 
     @Override public List<NepBlueprint> findAll() {
-        return new ArrayList<>(nepBlueprintHashMap.values());
+        lock.readLock().lock();
+        try {
+            return new ArrayList<>(nepBlueprintHashMap.values());
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     @Override public void registerBlueprint(NepBlueprint blueprint) {
-        nepBlueprintHashMap.put(blueprint.getNetworkId(), blueprint);
+        lock.writeLock().lock();
+        try {
+            nepBlueprintHashMap.put(blueprint.getNetworkId(), blueprint);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 }
