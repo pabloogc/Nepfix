0c591abbed7c53005d9af3e56bca6cf37e2672d8
Pablo Orgaz <pabloogc@gmail.com>
2015-02-15 17:48:58 +0100

    Local simulator execution from console

--- sim/src/main/java/com/nepfix/sim/App.java
+++ sim/src/main/java/com/nepfix/sim/App.java
@@ -1,27 +1,40 @@
 package com.nepfix.sim;
 
 
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.nepfix.sim.nep.Nep;
 import com.nepfix.sim.nep.NepBlueprint;
 import com.nepfix.sim.nep.NepReader;
 import com.nepfix.sim.request.ComputationRequest;
 
-import java.io.InputStreamReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class App {
 
-    public static void main(String[] args) {
-        String nepFile = "fizzbuzz.json";
-        NepBlueprint blueprint = NepReader.load(new InputStreamReader(App.class.getClassLoader().getResourceAsStream(nepFile)));
+    public static void main(String[] args) throws IOException {
+
+        if (!(args.length == 2 || args.length == 4))
+            throw new IllegalArgumentException("Usage: sim nep_file.json computation_request.json [-o output_file]");
+
+        NepBlueprint blueprint = NepReader.load(new FileReader(args[0]));
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        ComputationRequest request = gson.fromJson(new FileReader(args[1]), ComputationRequest.class);
+
         Nep nep = blueprint.create();
-        String input = "a";
-        for (int i = 0; i < 3000; i++) {
-            input += "a";
-        }
-        ComputationRequest request = new ComputationRequest(input, "1", 60*1000, 3000);
         List<String> result = nep.compute(request);
-        System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(result));
+
+        if (args.length == 4 && args[2].equals("-o")) { //Output to file
+            FileWriter fileWriter = new FileWriter(args[3]);
+            fileWriter.write(gson.toJson(result));
+            fileWriter.flush();
+        } else {
+            System.out.println(gson.toJson(result));
+        }
+
     }
 }
